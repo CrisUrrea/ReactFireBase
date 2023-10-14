36874dbd2076f7ddc5b3619d83af85a5dff920e3
CrisUrrea <cristhianca2009@gmail.com>
2023-10-14 16:02:10 -0500

    primer envio

---
+++ src/credenciales.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDck1bLwwXnkPwkZW4n3SMKi_JQ6R8i_Zk",
+  authDomain: "fir-reactu.firebaseapp.com",
+  projectId: "fir-reactu",
+  storageBucket: "fir-reactu.appspot.com",
+  messagingSenderId: "57097236612",
+  appId: "1:57097236612:web:635c59e1f5bae0264212d1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
