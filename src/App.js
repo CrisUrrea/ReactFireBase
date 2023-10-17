93840413fe2c5121b12386e1d2f1be8f6d07b170
CrisUrrea <cristhianca2009@gmail.com>
2023-10-17 15:24:52 -0500

    Creacion Main y Login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import React, {useState} from 'react';
 import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
 
 function App() {
+  const [usuario, setUsuario] = useState(null);
   return (
-    <div></div>
+    <div className=''>
+      {usuario ? <Home/> : <Login/>}
+    </div>
   );
 }
 
